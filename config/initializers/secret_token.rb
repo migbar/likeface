604a2f3fff5303eb33df771544b524e6eec1bdda
Miguel Barcos <miguel@galar.com>
2011-10-18 19:18:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Likeface::Application.config.secret_token = '3d7962002b7a7b29b8ae54fdc9153c4407d76f928d413163b198f0ea0b78452a958155335076cea602665c6fc462d036613f8b920098fa88577e19aa68dcba9c'
